208ec024d646a77f08e36a09e484bbfa5bdc55a1
j-chimienti <jchimien@mail.usf.edu>
2017-12-13 08:37:00 -0500

    python serve

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
 
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 
 from solver import solve, time_solve
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='build/static')
 
 
-@app.route('/api/solve', methods=['POST'])
+@app.route('/solve', methods=['POST'])
 def solve():
     json = request.get_json()
 
-    # print(json)
+    if "grid" in json:
+        grid = json['grid']
 
-    grid = json['grid']
+        result = time_solve(grid)
 
-    result = time_solve(grid)
+        return jsonify(result), 200
 
-    return jsonify(result), 200
+    return jsonify('bad request'), 400
+
+
+@app.route('/', methods=['get'])
+def serve():
+
+    return send_from_directory('build', 'index.html')
 
 
 if __name__ == '__main__':
@@ -28,4 +35,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     port = args.port
 
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, threaded = True, use_reloader = True)
